41f21c5d82b8a0c3f4ff40f91a3d349fbb4ed569
TRANCONGMINH <tranminh1134@gmail.com>
2018-09-15 16:38:15 +0700

    Bai tap buoi 2

--- ExerciseSS2/index.php
+++ ExerciseSS2/index.php
@@ -1,41 +1,13 @@
 <?php
-
-	//Khai báo một mảng
-	// $arr = [];
-	// $arr = ['Minh', 'Tai', 'Nhu Y', 'Suong', 'Thang'];
-	// $arr2 = [1 => 'Minh', 2 => 'Tai', 3 => 'Nhu Y', 4 => 'Suong', 5 => 'Thang'];// key mặc định
-	// $arr3 = [22 => 'Minh', 20 => 'Tai', 35 => 'Nhu Y', 41 => 'Suong', 78 => 'Thang'];// key mặc tự đặt
-
-	// var_dump($arr);
-	// echo "<br>";
-	// var_dump($arr2);
-	// echo "<br>";
-	// foreach ($arr as $key => $value) {// Vòng lặp foreach dùng để duyệt và in các phần tử trong mảng
-	// 	echo "$key =>"."$value";// in ra key and value
-	// 	echo "<br>";
-	// 	echo "$value";// in ra value
-	// 	echo "<br>";
-	// }// In mảng 
-	// array_push($arr,'Nhi');// Đẩy thêm 1 phần tử vào cuối mảng
-	// var_dump($arr);
-	// echo "<br>";
-	// array_pop($arr);// Bỏ 1 phần tử ở cuối mảng
-	// var_dump($arr);
-	// echo "<br>";
-	// unset($arr['0']);// bỏ 1 phần tử
-	// var_dump($arr);
-	// $arr4 = array_merge($arr2, $arr3);
-	// var_dump($arr3);
-	// echo "<br>";
 	$Smartphone = array (
 		'Iphone X ' => array (
 			'name'   => 'Apple iphone X',
-			'gia'    => '200000000',
+			'gia'    => '999',
 			'image'	 => 'iphone-x-64gb.png'
 		),
 		'Galaxy J8 ' => array (
 			'name'   => 'SamSung Galaxy J8',
-			'gia'    => '60000000',
+			'gia'    => '499',
 			'image'	 => 'samsung-galaxy-j8.png'
 		),
 	);
@@ -45,7 +17,7 @@
 		$hinhanh = $value['image'];
 		echo "$i "."$key"."<br>";
 		echo $value['name']." Giá ";
-		echo $value['gia']."<br>";
+		echo $value['gia']."$ "."<br>";
 		echo "<b>Hình ảnh : </b> <img src=".$hinhanh." />"."<br>";
 		$i++;
 	}// Hàm in mảng
@@ -60,8 +32,8 @@
 		$hinhanh = $value['image'];
 		echo "$i "."$key"."<br>";
 		echo $value['name']."   Giá  ";
-		echo $value['gia']."</br>    Giá bán còn   ";
-		echo giamGia($value['gia'],$value['giamgia']);
+		echo $value['gia']."$ "."</br>    Giá bán còn   ";
+		echo giamGia($value['gia'],$value['giamgia'])."$ ";
 		echo "   Đã giảm giá  ".$value['giamgia']."  %  </br>";
 		echo "<b>Hình ảnh : </b> <img src=".$hinhanh." />"."<br>";
 		$i++;
@@ -85,22 +57,9 @@
 	echo "|| Thêm sản phẩm Xiaomi ||</br>";
 	array_push($array,'Xiaomi ');
 	$Smartphone['Xiaomi ']['name'] = 'Xiaomi Mi 8';
-	$Smartphone['Xiaomi ']['gia'] = '130000000';
+	$Smartphone['Xiaomi ']['gia'] = '799';
 	$Smartphone['Xiaomi ']['image'] = 'xiaomi-mi-8.png';
 	$Smartphone['Xiaomi ']['giamgia'] = '15';
 	danhSmartphoneGiamGia($Smartphone);
-	// unset($Smartphone['Tai']); // xóa bạn Tài
-	// $arrayVuong = array (
-	// 	'Vuong' => array (
-	// 		'name'   => 'Vuong',
-	// 		'age'    => '26',
-	// 		'gender' => 'orther',
-	// 		'phone'	 => '0983960455'
-	// 	)
-	// );
-	// $Smartphone = array_merge($Smartphone, $arrayVuong); // Thêm bạn Vương
-	// $Smartphone['Minh ']['phone'] = '99999999';// Đổi số điện thoại bạn Min
-	// danhSachLop($Smartphone);
-
 
 ?>
