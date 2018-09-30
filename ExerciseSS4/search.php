04657c3cab3d4e2c2b7af746233e1fbeeef32d87
TRANCONGMINH <tranminh1134@gmail.com>
2018-09-30 08:53:08 +0700

    EX4

--- ExerciseSS4/search.php
+++ ExerciseSS4/search.php
@@ -14,14 +14,14 @@
 	if(isset($_POST['submit'])){
 	$keywork = $_POST['keywork'];
 	$sql_s = "SELECT * FROM product
-				WHERE name LIKE '%$keywork%'";
+				WHERE name LIKE '%$keywork%' or description LIKE '%$keywork%'";
 	$rs=mysqli_query($conn,$sql_s);	
 	if ($keywork == '') {
 		echo "Mời bạn nhập từ khóa "; 
-		} else
-	while($row = mysqli_fetch_array($rs, MYSQLI_NUM))
+		}else
+			while($row = mysqli_fetch_array($rs, MYSQLI_NUM))
 	  		 {
-	  		 	echo "--ID :" .$row[0] ."</br> Name product :".$row[1] ."</br> Price product :".$row[2]."$"."</br> Description product :".$row[3];
+	  		 echo "--ID :" .$row[0] ."</br> Name product :".$row[1] ."</br> Price product :".$row[2]."$"."</br> Description product :".$row[3];
 	}
 	}
 	?>
